537d5a01752042fc78eff2ee37c1f45044ab2dec
Andreas Ländle <alaendle@freenet.de>
2013-11-05 23:30:25 +0100

    Fixed problem of recursive parsing of the CategoryItem child's.

--- Android/MagicOfWinter/src/com/outdooractive/api/CategoryItem.scala
+++ Android/MagicOfWinter/src/com/outdooractive/api/CategoryItem.scala
@@ -12,17 +12,15 @@ import org.json4s.JsonAST.JString
 import org.json4s.JsonAST.JValue
 import org.json4s.native.JsonParser
 
-class CategoryItem(x: JValue) {
-  def this(jsonString: String) = this(JsonParser.parse(jsonString))
+class CategoryItem(val id: String, val name: String, childJson: JValue) {
+  def this(jsonString: String) = this("0", "root", JsonParser.parse(jsonString) \ "category")
 
   implicit lazy val formats = DefaultFormats
-  val id = (x \ "id").extractOpt[String] getOrElse "0"
-  val name = (x \ "name").extractOpt[String] getOrElse "root"
   val children: java.util.List[CategoryItem] =
-    for {
-      JArray(categories) <- (x \ "category")
-      category <- categories
-    } yield new CategoryItem(category)
+    childJson match {
+      case JArray(categories) => categories.map(x => new CategoryItem((x \ "id").extract[String], (x \ "name").extract[String], x \ "category"))
+      case _ => Nil
+    }
 
   def findById(id: String): CategoryItem = {
     if (this.id == id) {
